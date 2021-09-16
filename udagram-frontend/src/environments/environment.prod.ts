8bb8ffeb39435cc6cbc3688f577e35466ea2ef69
mo2274 <mohamedzaghlol74111@gmail.com>
2021-09-16 20:32:23 +0200

    fix the error in environment.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a1a638617f38841a7a2d9f76efbb1864-599234930.us-east-1.elb.amazonaws.com:8080/api/v0''
+  apiHost: 'http://a1a638617f38841a7a2d9f76efbb1864-599234930.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
